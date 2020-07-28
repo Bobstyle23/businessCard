fce9ea1c76de0d1dc195d43f4fba7b05794ccece
Mukhammad Bobur Pakhriev <pahriyev4888@mail.ru>
2020-07-28 19:55:55 +0900

    Initial commit of Business card

---
+++ BobBizCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  BobBizCard
+//
+//  Created by MukhammadBobur Pakhriyev on 2020/06/08.
+//  Copyright © 2020 MukhammadBobur Pakhriyev. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 55)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
